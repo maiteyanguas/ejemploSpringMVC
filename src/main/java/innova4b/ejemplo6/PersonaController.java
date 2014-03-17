10cbc48ffa873519f38f85c2d43f20a0849fc829
Maite <maite.yanguas@gmail.com>
2014-03-17 19:24:58 +0100

    Controlador 1: Devuelve nombre de vista. Recibe ModelMap

--- src/main/java/innova4b/ejemplo6/PersonaController.java
+++ src/main/java/innova4b/ejemplo6/PersonaController.java
@@ -2,20 +2,22 @@ package innova4b.ejemplo6;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
 import innova4b.ejemplo6.Persona;
 
 @Controller
 @RequestMapping("/persona")
 public class PersonaController {
-
+	
 	@RequestMapping(value = "/show", method = RequestMethod.GET)
-	public String ver(ModelMap model) {
+	public String show(ModelMap model) {
+		model.addAttribute("persona", buildPersona());
+		return "persona/show";
+
+	}
+	
+	private Persona buildPersona(){
 		Persona persona = new Persona();
 		String nombre = "Maite";
 		String apellido = "Yanguas";
@@ -23,25 +25,10 @@ public class PersonaController {
 		persona.setNombre(nombre);
 		persona.setApellido(apellido);
 		persona.setEdad(edad);
-
-		model.addAttribute("persona", persona);
-		return "persona";
+		return persona;
 	}
 	
-	@RequestMapping(value = "/new", method = RequestMethod.GET)
-	public ModelAndView nueva() {
-		return new ModelAndView("personaNueva", "command", new Persona());
-	}
 	
-	@RequestMapping(value = "/add", method = RequestMethod.POST)
-	public String nueva(@ModelAttribute("SpringWeb")Persona persona, ModelMap model) {
-		Persona nuevaPersona = new Persona();
-		nuevaPersona.setNombre(persona.getNombre());
-		nuevaPersona.setApellido(persona.getApellido());
-		nuevaPersona.setEdad(persona.getEdad());
 
-		model.addAttribute("persona", nuevaPersona);
-		return "persona";
-	}
 
 }
