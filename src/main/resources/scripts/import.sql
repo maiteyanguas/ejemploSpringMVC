4b3ed28c9bbee2e94c211d6daa2de61b51965365
Maite <maite.yanguas@gmail.com>
2014-04-20 10:41:16 +0200

    creacion automatica de tablas en hibernate

---
+++ src/main/resources/scripts/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO `persona` (`id`,`nombre`,`apellido`,`fechaNacimiento`) VALUES (1,'Aimar','Olaizola','1977-10-15');
+INSERT INTO `persona` (`id`,`nombre`,`apellido`,`fechaNacimiento`) VALUES (2,'Pablo','Berasaluze','1979-05-24');
+INSERT INTO `persona` (`id`,`nombre`,`apellido`,`fechaNacimiento`) VALUES (3,'David','Merino','1989-02-21');
+INSERT INTO `persona` (`id`,`nombre`,`apellido`,`fechaNacimiento`) VALUES (4,'Julian','Retegi','1972-12-13');
+INSERT INTO `persona` (`id`,`nombre`,`apellido`,`fechaNacimiento`) VALUES (5,'Iñaki','Perurena','1987-12-05');
